ea976a2d2c62ce77e72756a21e95cd011bb102c4
Anton B <antonbsa.bck@gmail.com>
2025-06-11 10:41:00 -0300

    feat(tests): add data channel plugin tests with increment and wipe functionality

---
+++ samples/sample-data-channel-plugin/tests/elements.ts
@@ -0,0 +1,7 @@
+import { coreElements } from '../../../tests/core/coreElements';
+
+export const elements = {
+  ...coreElements,
+  incrementDataChannelButtonPlugin: 'li[data-test="incrementDataChannelButtonPlugin"]',
+  wipeDataOffButtonPlugin: 'li[data-test="wipeDataOffButtonPlugin"]',
+};
