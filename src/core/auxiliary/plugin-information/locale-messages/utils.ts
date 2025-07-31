222eb345137ab010bd3a3583d057b3fcdd0e514c
Guilherme Leme <leme.guilherme.p@gmail.com>
2025-07-31 09:20:20 -0300

    [fix-use-locales] fixed useLocale not finding similar language amongst the possible ones

--- src/core/auxiliary/plugin-information/locale-messages/utils.ts
+++ src/core/auxiliary/plugin-information/locale-messages/utils.ts
@@ -1,3 +1,8 @@
+import { useEffect, useState } from 'react';
+import { DataWaitingWrapper, UseLocaleMessagesProps } from './types';
+import { IntlLocaleUiDataNames } from '../../../../ui-data';
+import { LocaleObject } from '../../../../ui-data/domain/intl/locale/types';
+
 async function fetchLocaleAndStore(
   localeUrl: string,
   fetchConfigs?: RequestInit,
@@ -14,4 +19,104 @@ function mergeLocaleMessages(
   return { ...fallbackMessages, ...desiredMessages };
 }
 
-export { fetchLocaleAndStore, mergeLocaleMessages };
+function useGetLocalesIndex(
+  localeUrl: string | undefined,
+  fetchConfigs?: RequestInit,
+): DataWaitingWrapper<string[]> {
+  const [indexOfLocales, setIndexOfLocales] = useState<DataWaitingWrapper<string[]>>({
+    data: [],
+    loading: true,
+  });
+
+  useEffect(() => {
+    if (localeUrl || localeUrl !== '') {
+      const indexUrl = `${localeUrl}/index.json`;
+      fetch(indexUrl, fetchConfigs)
+        .then((res) => res.json())
+        .then((list: string[]) => {
+          const filtered = list.filter((filename) => filename !== 'index.json');
+          setIndexOfLocales({
+            data: filtered,
+            loading: false,
+          });
+        })
+        .catch((error) => {
+          setIndexOfLocales({
+            data: [],
+            loading: false,
+            error,
+          });
+        });
+    }
+  }, [localeUrl]);
+
+  return indexOfLocales;
+}
+
+function useGetNormalizedLocale(
+  { pluginApi, fetchConfigs }: UseLocaleMessagesProps,
+): DataWaitingWrapper<LocaleObject> {
+  const localeFromUiData = pluginApi.useUiData!(IntlLocaleUiDataNames.CURRENT_LOCALE, {
+    locale: 'en',
+    fallbackLocale: 'en',
+  });
+
+  const [
+    currentLocale,
+    setCurrentLocale,
+  ] = useState<DataWaitingWrapper<LocaleObject>>({
+    data: localeFromUiData,
+    loading: true,
+  });
+
+  const localesIndex = useGetLocalesIndex(
+    pluginApi.localesBaseUrl,
+    fetchConfigs,
+  );
+
+  useEffect(() => {
+    if (!localesIndex.loading && !localesIndex.error) {
+      const desiredLocale = localeFromUiData.locale.split(/[-_]/g);
+
+      const usableLocales = localesIndex.data
+        .map((file) => file.replace('.json', ''))
+        .reduce((locales: string[], locale: string) => (locale.match(desiredLocale[0])
+          ? [...locales, locale]
+          : locales), []);
+
+      const isDesiredLocalePresent = usableLocales.findIndex(
+        (locale) => locale === localeFromUiData.locale,
+      ) !== -1;
+
+      if (isDesiredLocalePresent && localeFromUiData.locale !== currentLocale.data.locale) {
+        setCurrentLocale({
+          data: localeFromUiData,
+          loading: false,
+        });
+      } else if (!isDesiredLocalePresent) {
+        const regionDefault = usableLocales.find((locale) => locale === desiredLocale[0]);
+        const localeFallback = regionDefault || usableLocales[0];
+        setCurrentLocale({
+          data: {
+            ...localeFromUiData,
+            locale: localeFallback,
+          },
+          loading: false,
+        });
+      }
+    } else if (localeFromUiData.locale !== currentLocale.data.locale) {
+      setCurrentLocale({
+        data: localeFromUiData,
+        loading: false,
+      });
+    }
+  }, [localeFromUiData, localesIndex]);
+  return currentLocale;
+}
+
+export {
+  fetchLocaleAndStore,
+  mergeLocaleMessages,
+  useGetLocalesIndex,
+  useGetNormalizedLocale,
+};
